18ecc1514e0818a9291d5483f96e736145f025a7
Mathew Cook <mcook@savvysherpa.com>
2021-06-29 20:09:13 -0500

    react query playground

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "react-query";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export const App = () => {
+  const { data, isLoading } = useQuery("planets", () => {
+    return axios
+      .get("https://swapi.dev/api/planets")
+      .then((res) => res.data.results);
+  });
+  console.log("DATA", data);
+  return isLoading ? (
+    <h1>Loading .....</h1>
+  ) : (
+    <div>
+      {data.map((planet) => (
+        <h1 key={planet.name}>{planet.name}</h1>
+      ))}
     </div>
   );
-}
-
-export default App;
+};
